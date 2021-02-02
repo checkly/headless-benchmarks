8c74583f8934c46037e03429463fe7cc8f0db76c
Giovanni Rago <ragog90@gmail.com>
2021-02-02 17:34:45 +0100

    refactor: additional restructuring

--- bench-checkly-suites-cypress.sh
+++ bench-checkly-suites-cypress.sh
@@ -5,6 +5,6 @@ export TIMEFMT=$'%*E'
 
 # run argument_1 times and print time result to file with id argument_2 
 for i in {1..$1}; do
- { echo $i && time ./node_modules/.bin/cypress run --spec "scripts/cypress/checkly-check.js" } 2>> results/performance/cypress-checkly-$1-$2.txt
+ { echo $i && time ./node_modules/.bin/cypress run --spec "scripts/cypress/checkly-*.js" } 2>> results/performance/cypress-checkly-$1-$2.txt
  echo $? >> results/exit-status/cypress-checkly-$1-$2-status.txt
 done
--- bench-danube-cypress.sh
+++ bench-danube-cypress.sh
@@ -1,10 +1,14 @@
 #!/bin/zsh
 
+# test data for https://danube-webshop.herokuapp.com
+export CYPRESS_USER_EMAIL=user@email.com
+export CYPRESS_USER_PASSWORD=supersecure1
+
 # format to only show total seconds
 export TIMEFMT=$'%*E'
 
 # run argument_1 times and print time result to file with id argument_2 
 for i in {1..$1}; do
- { echo $i && time ./scripts/node_modules/.bin/cypress run } 2>> results/performance/cypress-danube-$1-$2.txt
+ { echo $i && time ./node_modules/.bin/cypress run --spec "scripts/cypress/danube-login.js" } 2>> results/performance/cypress-danube-$1-$2.txt
  echo $? >> results/exit-status/cypress-danube-$1-$2-status.txt
 done
